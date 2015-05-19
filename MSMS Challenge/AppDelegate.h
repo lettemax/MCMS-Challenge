cc6372b87528a58349112722d2d4236b9b7efa2e
lettemax <maxlettenberger@gmail.com>
2015-05-19 14:02:23 -0700

    Initial Commit

---
+++ MSMS Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MSMS Challenge
+//
+//  Created by Max Lettenberger on 5/19/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MSMS Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSMS Challenge
+//
+//  Created by Max Lettenberger on 5/19/15.
+//  Copyright (c) 2015 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
